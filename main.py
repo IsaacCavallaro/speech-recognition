88f43c6811e4b208249997c6a01f5ea812170c0e
isaaccavallaro <isaaccavallaro@gmail.com>
2022-06-18 09:40:53 +1000

    Adding transcribe functionality

--- main.py
+++ main.py
@@ -4,19 +4,35 @@ import sys
 
 #upload
 upload_endpoint = 'https://api.assemblyai.com/v2/upload'
+transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
 filename = sys.argv[1]
-
-def read_file(filename, chunk_size=5242880):
-    with open(filename, 'rb') as _file:
-        while True:
-            data = _file.read(chunk_size)
-            if not data:
-                break
-            yield data
-
 headers = {'authorization': API_KEY_ASSEMBLY_API}
-response = requests.post(upload_endpoint,
-                        headers=headers,
-                        data=read_file(filename))
 
-print(response.json())
+def upload(filename):
+    def read_file(filename, chunk_size=5242880):
+        with open(filename, 'rb') as _file:
+            while True:
+                data = _file.read(chunk_size)
+                if not data:
+                    break
+                yield data
+
+    upload_response = requests.post(upload_endpoint,
+                            headers=headers,
+                            data=read_file(filename))
+
+    audio_url = upload_response.json()['upload_url']
+    return audio_url
+
+# transcribe
+def transcribe(audio_url):
+    transcript_request = { "audio_url": audio_url }
+    transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
+    job_id = transcript_response.json()['id']
+    return job_id
+
+audio_url = upload(filename)
+job_id = transcribe(audio_url)
+
+print(job_id)
+#poll
